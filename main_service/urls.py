9082b4a1ceb802b592dceec496a0517423a87ddc
raziyeh303 <raziyeh303@gmial.coml>
2021-11-26 22:39:50 +0330

    complete get data by their rank

--- main_service/urls.py
+++ main_service/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from main_service.views.data_sales import AllDataSales
+from main_service.views.detection_data_by_rank import GetDataByRank
 
 urlpatterns = [
     path('all_data/', AllDataSales.as_view(), name='get_all_data'),
-
+    path('data_by_rank/<int:pk>/', GetDataByRank.as_view(), name='data_by_rank'),
 ]
