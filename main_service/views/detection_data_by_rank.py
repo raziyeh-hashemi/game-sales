d2a0b0fde84c75b314ec1322b7bc3b8ab6f78d5f
raziyeh sadat hashemi <45314636+raziyeh-hashemi@users.noreply.github.com>
2021-11-26 22:42:07 +0330

    complete get data by their rank (#4)
    Co-authored-by: raziyeh303 <raziyeh303@gmial.coml>

--- main_service/views/detection_data_by_rank.py
+++ main_service/views/detection_data_by_rank.py
@@ -3,4 +3,15 @@ from utils.response_handler import StandardizedResponse
 from main_service.models.data_sales import DataSales
 from main_service.serializers.data_sales import DataSalesSerializer
 
+
 class GetDataByRank(APIView):
+
+    def get(self, request, pk):
+        query_by_rank = DataSales.objects.filter(rank=pk)
+        if not query_by_rank:
+            return StandardizedResponse(success=False, status_code=404,
+                                        message='OOPS! There is no game with this rank')
+        for obj in query_by_rank:
+            serializer = DataSalesSerializer(obj)
+        return StandardizedResponse(success=True, status_code=200, data=serializer.data,
+                                    message='The game with requested rank is found successfully')
